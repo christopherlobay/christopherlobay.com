1543d23814507601d5ff4321dc44aaa3311c479c
Christopher Lobay <hello@christopherlobay.com>
2018-05-18 16:38:34 -0400

    Simplify

--- js/email.js
+++
@@ -1,9 +0,0 @@
-var ul = document.getElementsByTagName('ul')[0],
-    li = document.createElement('li'),
-    a = document.createElement('a'),
-    href = 'bWFpbHRvOmhlbGxvQGNocmlzdG9waGVybG9iYXkuY29t';
-
-a.setAttribute('href', atob(href));
-a.textContent = 'Email';
-li.appendChild(a);
-ul.insertBefore(li, ul.firstChild);
